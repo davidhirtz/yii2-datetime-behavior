0f7cdb1a6142db74c1ef4958deea3680c097fbfd
davidhirtz <david.hirtz@me.com>
2020-12-08 23:29:32 +0100

    Update DateTimeValidator.php

--- DateTimeValidator.php
+++ DateTimeValidator.php
@@ -57,14 +57,29 @@ class DateTimeValidator extends \yii\validators\Validator
     {
         if (!$model->{$attribute} instanceof $this->dateClass) {
             try {
-                // Replace European date dd.mm.yy with US mm/dd/yy
-                $model->{$attribute} = @new $this->dateClass(preg_replace_callback('#^(\d{1,2})\.(\d{1,2})\.?(\d{0,4})#', function ($matches) {
-                    return $matches[2] . '/' . $matches[1] . '/' . ($matches[3] ?: date('Y'));
-                }, $model->{$attribute}), $this->timezone);
-
+                $datetime = $this->formatDateTime($model->{$attribute});
+                $model->{$attribute} = @new $this->dateClass($datetime, $this->timezone);
             } catch (\Exception $ex) {
                 $this->addError($model, $attribute, $this->message);
             }
         }
+
+        // If the date hasn't changed, set the attribute to the old record so it passes Yii's `isAttributeChanged`
+        // method which requires the DateTime objects to be identical and thus will not be updated on save.
+        if ($model->{$attribute} == $model->getOldAttribute($attribute)) {
+            $model->{$attribute} = $model->getOldAttribute($attribute);
+        }
+    }
+
+    /**
+     * Replaces European date format dd.mm.yy with US mm/dd/yy.
+     * @param string $datetime
+     * @return string|string[]|null
+     */
+    protected function formatDateTime($datetime)
+    {
+        return preg_replace_callback('#^(\d{1,2})\.(\d{1,2})\.?(\d{0,4})#', function ($matches) {
+            return $matches[2] . '/' . $matches[1] . '/' . ($matches[3] ?: date('Y'));
+        }, $datetime);
     }
 }
